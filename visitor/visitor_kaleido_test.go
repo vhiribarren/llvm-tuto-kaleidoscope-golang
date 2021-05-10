ce19fd6c285b28c02c7b57760239fe841139a7e3
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2021-05-10 22:23:43 +0200

    One little more test

--- visitor/visitor_kaleido_test.go
+++ visitor/visitor_kaleido_test.go
@@ -42,4 +42,24 @@ func TestParseValidProgram(t *testing.T) {
 }
 
 func TestParseInvalidProgram(t *testing.T) {
+	entries, err := os.ReadDir(invalidProgramDirectory)
+	if err != nil {
+		t.Error(err)
+	}
+	for _, entry := range entries {
+		file := path.Join(invalidProgramDirectory, entry.Name())
+		fileContent, err := os.ReadFile(file)
+		if err != nil {
+			t.Error("File", file, err)
+		}
+		ast, err := yacc.BuildKaleidoAST(string(fileContent))
+		if err != nil {
+			t.Error("File", file, err)
+		}
+		visitor := NewVisitorKaleido()
+		_, err = visitor.GenerateIR(ast)
+		if err == nil {
+			t.Error("Should have triggered an error, file", file, err)
+		}
+	}
 }
